e07eac9e0becf264a8c93a39a9d34f6335095c1d
derteta <derteta@gmx.de>
2017-07-12 22:46:19 +0200

    Remove command-t

--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@
 # Set up references to rc files
 echo "source ~/.vim/.vimrc" > ~/.vimrc
 echo "source ~/.vim/.gvimrc" > ~/.gvimrc
-
-# Compile plugins
-(cd ~/.vim/bundle/command-t/ruby/command-t && ruby extconf.rb && make)

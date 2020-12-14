0d3bfd6eaa4778292d9dcffc578aaea610f1c6d5
derteta <derteta@gmx.de>
2020-12-15 00:01:13 +0100

    setup: fix fzf path

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git submodule update --init --recursive
-(cd bundle/fzf && ./install --bin)
+(cd pack/navigation/start/fzf && ./install --bin)
 
 # Set up references to rc files
 echo "source ~/.vim/.vimrc" > ~/.vimrc
